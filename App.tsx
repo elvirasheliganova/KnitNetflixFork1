0204d879e72ef5beafecaa3041eb6158e635c0a8
Elvira Shelyganova <elvirashelyganova@gmail>
2021-12-03 16:43:44 +0100

    customAuthUI

--- App.tsx
+++ App.tsx
@@ -1,17 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Amplify, { Auth, API, graphqlOperation } from "aws-amplify";
-import { withAuthenticator } from 'aws-amplify-react-native'
+import { withAuthenticator } from 'aws-amplify-react-native';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+//import { Authenticator } from 'aws-amplify-react-native'
+import { AmplifyTheme } from './/src/AmplifyTheme'
 import awsconfig from ".//src/aws-exports";
 Amplify.configure(awsconfig);
 
 
 
 function App() {
+  {/*const signUpConfig = {
+    hideAllDefaults: true,
+    signUpFields: [
+      {
+        label: 'Email',
+        key: 'email',
+        required: true,
+        displayOrder: 1,
+        type: 'string',
+      },
+      {
+        label: 'Password',
+        key: 'password',
+        required: true,
+        displayOrder: 2,
+        type: 'password',
+      },
+    ],
+  }*/}
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -19,11 +42,13 @@ function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      //<Authenticator usernameAttributes="email" signUpConfig={signUpConfig} theme={AmplifyTheme} >
+          <SafeAreaProvider>
+            <Navigation colorScheme={colorScheme} />
+            <StatusBar />
+          </SafeAreaProvider>
+      //</Authenticator>
     );
   }
 }
-export default withAuthenticator(App)
+export default withAuthenticator(App, false, [], null, AmplifyTheme)
